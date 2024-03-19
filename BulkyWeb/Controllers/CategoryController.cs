c3507af5703f17484da0939143bbcfa8b7a09c59
ankitasharma-18 <ankitasharma199818@gmail.com>
2024-03-19 01:11:45 -0400

    Implemented CRUD operations for categories

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,12 +1,96 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _context = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _context.Categories.ToList();
+            return View(objCategoryList);
         }
-    }
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+        [HttpPost]
+		public IActionResult Create(Category obj)
+		{
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder Cannot  match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+				_context.Categories.Add(obj);
+				_context.SaveChanges();
+				return RedirectToAction("Index");
+			}          
+			return View();
+
+		}
+		public IActionResult Edit(int? id)
+		{
+			if(id==null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _context.Categories.FirstOrDefault(u => u.Id == id);
+			if(categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+
+		}
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			
+			if (ModelState.IsValid)
+			{
+				_context.Categories.Update(obj);
+				_context.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+
+		}
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _context.Categories.FirstOrDefault(u => u.Id == id);
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+
+		}
+		[HttpPost,ActionName("Delete")]
+		public IActionResult DeletePost(int? id)
+		{
+			Category obj = _context.Categories.Find(id);
+			if(obj== null)
+			{
+				return NotFound();
+			}
+	
+				_context.Categories.Remove(obj);
+				_context.SaveChanges();
+				return RedirectToAction("Index");
+
+		}
+	}
 }
